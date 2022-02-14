9ca41f41b134303183a3d638addcfb642428db06
Jonathan Manning <jmanning@ebi.ac.uk>
2022-02-14 15:05:35 +0000

    Update required analysis

--- atlas_anndata/funcs.py
+++ atlas_anndata/funcs.py
@@ -175,8 +175,8 @@ def validate_anndata_with_config(anndata_config, anndata_file):
 
     # Check that some necessary version info is present
 
-    analysis_names = [x["analysis"] for x in config["analysis_versions"]]
-    required_analyses = ["reference", "alignment"]
+    analysis_names = [x["analysis"].lower() for x in config["analysis_versions"]]
+    required_analyses = ["reference", "mapping"]
     if not set(required_analyses).issubset(analysis_names):
         errmsg = (
             f"At least {required_analyses} must be described in"
